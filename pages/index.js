da9fb4af684ef6db4ebcbf5ba4390e5b48998d42
manlikexavy <chibokaxavier@gmail.com>
2023-05-27 04:19:06 -0700

    BODY COMPLETED, UP NEXT FOOTER

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Avatar from "@/components/Avatar";
 import Head from "next/head";
 import Image from "next/image";
 import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Home() {
   return (
@@ -28,6 +29,22 @@ export default function Home() {
       </header>
 
       {/* body */}
+      <form className="mt-44 flex flex-col items-center flex-grow ">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md lg:max-w-2xl px-5 py-3 items-center sm:max-w-xl rounded-full border border-gray-200">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="focus:outline-none flex-grow" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8  sm:space-y-0 sm:flex-row  sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm feeling lucky</button>
+        </div>
+      </form>
       {/* footer */}
     </>
   );
